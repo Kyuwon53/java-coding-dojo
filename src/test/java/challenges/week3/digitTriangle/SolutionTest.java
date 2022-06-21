2e3bd1ca8ccf1a79f80c548ed6f355d54788c632
kyuwon53 <lkw2v@naver.com>
2022-06-21 21:43:46 +0900

    정수 삼각형 테스트 작성

--- src/test/java/challenges/week3/digitTriangle/SolutionTest.java
+++ src/test/java/challenges/week3/digitTriangle/SolutionTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package challenges.week3.digitTriangle;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SolutionTest {
-  
+    final Solution solution = new Solution();
+
+    @Test
+    @DisplayName("5층 삼각형")
+    void triangle_5nd_floor() {
+        int[][] triangle = {
+                {7},
+                {3, 8},
+                {8, 1, 0},
+                {2, 7, 4, 4},
+                {4, 5, 2, 6, 5}
+        };
+        assertEquals(30, solution.solution(triangle));
+    }
+
+    @Test
+    @DisplayName("3층 삼각형")
+    void triangle_3nd_floor() {
+        int[][] triangle = {
+                {7},
+                {3, 8},
+        };
+        assertEquals(15, solution.solution(triangle));
+    }
 }
