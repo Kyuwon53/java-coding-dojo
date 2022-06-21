d244e2d3b7eae9a438a7099ed8115419c6a891ce
kyuwon53 <lkw2v@naver.com>
2022-06-21 21:44:16 +0900

    정수 삼각형 풀이 작성

--- src/main/java/challenges/week3/digitTriangle/Solution.java
+++ src/main/java/challenges/week3/digitTriangle/Solution.java
@@ -1,2 +1,20 @@
-package challenges.week3.digitTriangle;public class Solution {
+package challenges.week3.digitTriangle;
+
+public class Solution {
+    public int solution(int[][] triangle) {
+
+        int n = triangle.length;
+
+        int[][] sumTriangle = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            sumTriangle[n - 1][i] = triangle[n - 1][i];
+        }
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                sumTriangle[i - 1][j] = Math.max(sumTriangle[i][j] + triangle[i - 1][j], sumTriangle[i][j + 1] + triangle[i - 1][j]);
+            }
+        }
+
+        return sumTriangle[0][0];
+    }
 }
